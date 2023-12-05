78f0932404345275f21d440e3e2de051c329b9ca
Ahmed <ahmedzulkarnaev9500@gmail.com>
2023-12-05 14:55:53 +0300

    update code

--- homework.py
+++ homework.py
@@ -31,7 +31,9 @@ logger.addHandler(logging.StreamHandler(sys.stdout))
 
 class ApiError(Exception):
     """Исключение для ошибок при обращении к API."""
+
     def __init__(self, message):
+        """Сообщение об ошибке, описывающее причину исключения."""
         super().__init__(message)
         self.message = message
 
